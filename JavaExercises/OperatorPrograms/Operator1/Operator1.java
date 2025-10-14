097d6026f3ebb72d5905de6d7bdaaf82b214aba9
apurva-ranjan <text.apurva@gmail.com>
2025-10-14 17:31:57 +0530

    Create Operator1.java

---
+++ JavaExercises/OperatorPrograms/Operator1/Operator1.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Quotient: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+    }
+}
