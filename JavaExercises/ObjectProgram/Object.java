3f0f940f5b9ac8b1c5bd95943e03bdea50229d03
apurva-ranjan <text.apurva@gmail.com>
2025-10-14 18:02:36 +0530

    Update Object.java

--- JavaExercises/ObjectProgram/Object.java
+++ JavaExercises/ObjectProgram/Object.java
@@ -1,17 +1,23 @@
+class Book {
+    String title;
+    String author;
+    int pages;
+}
+
 public class Main {
     public static void main(String[] args) {
         // Creating an object of Book class
         Book myBook = new Book();
 
-        // Assigning values to fields
+        // Assigning values to object fields
         myBook.title = "Java Programming";
         myBook.author = "John Doe";
         myBook.pages = 350;
 
-        System.out.println("Book Title: " + myBook.title);
+        // Output
+        System.out.println("Book Details:");
+        System.out.println("Title: " + myBook.title);
         System.out.println("Author: " + myBook.author);
         System.out.println("Pages: " + myBook.pages);
     }
 }
-
-// Note: You will need the Book class defined somewhere accessible.
