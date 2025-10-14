390e91abf75648970f88d01f2aa92e921d3f97d9
apurva-ranjan <text.apurva@gmail.com>
2025-10-14 17:49:55 +0530

    Create Array2.java

---
+++ JavaExercises/ArrayProgram/Array2/Array2.java
@@ -0,0 +1,10 @@
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20};
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        System.out.println("Total Sum: " + sum);
+    }
+}
