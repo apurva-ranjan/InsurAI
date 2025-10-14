a8471ca2c6d915b6448f06c67a7fbe6abd1f2b61
apurva-ranjan <text.apurva@gmail.com>
2025-10-14 17:48:37 +0530

    Create Array1.java

---
+++ JavaExercises/ArrayProgram/Array1/Array1.java
@@ -0,0 +1,8 @@
+public class ArrayDeclaration {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element " + i + ": " + numbers[i]);
+        }
+    }
+}
