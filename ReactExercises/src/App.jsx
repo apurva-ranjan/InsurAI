124ab66bdd1f57c249485d58acc7468adc613f8a
apurva-ranjan <text.apurva@gmail.com>
2025-10-23 15:17:24 +0530

    Create App.jsx

---
+++ ReactExercises/src/App.jsx
@@ -0,0 +1,19 @@
+// src/App.jsx
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>React Exercise</h1>
+      </header>
+      <main>
+        <p>
+          This repository contains my implementation of the React exercise. Edit
+          components in <code>src/</code> and save to update.
+        </p>
+      </main>
+    </div>
+  );
+}
